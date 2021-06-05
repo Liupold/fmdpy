547fa28e3ca14344f0971de75ad7c380d065c06a
Liupold <rohnch@protonmail.ch>
2021-06-05 17:15:21 +0530

    version bump for  license fix

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmdpy",
-    version="0.1.r1",
+    version="0.1.r2",
     author="Rohn Chatterjee",
     author_email="rohn.ch@gmail.com",
     description="Music Downloader",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Liupold/fmdpy",
+    license='GPLv3',
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['ffmpeg-python', 'click', 'music-tag', 'requests', 'pillow'],
