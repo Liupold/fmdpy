5d19640261fc38a988ae7f506c33e6a9476a108a
Liupold <rohnch@protonmail.ch>
2022-05-26 18:49:24 +0530

    0.5.2 version bump.

--- fmdpy/__init__.py
+++ fmdpy/__init__.py
@@ -1,7 +1,7 @@
 """fmdpy."""
 from . import conf
 
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 install_requires = ['ffmpeg-python', 'click', 'music-tag>=0.4.3', 'requests',
                     'pillow', 'lyricsgenius', 'dataclasses', 'spotipy']
 
