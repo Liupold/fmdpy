2eacc8209e4151bde341613bf9fcbb38530d3f9e
Liupold <rohnch@protonmail.ch>
2022-06-05 14:01:11 +0530

    better way to handel cli options.

--- fmdpy/__main__.py
+++ fmdpy/__main__.py
@@ -22,30 +22,71 @@ except ModuleNotFoundError:
     sys.exit(2)
 
 
+def gen(ctx, _, value):
+    if not value:
+        return
+    else:
+        file_path = os.getenv('FMDPY_CONFIG_FILE') or \
+            os.path.expanduser('~/.fmdpy.ini')
+        with open(file_path, 'w', encoding='utf8') as configfile:
+            config.write(configfile)
+        click.echo(f"generated config into: {file_path}")
+        ctx.exit()
+
+def update(ctx, _, value):
+    if not value:
+        return
+    else:
+        subprocess.check_call([sys.executable, '-m', 'pip',
+            'install', '--upgrade', *install_requires])
+        ctx.exit()
+
+def version(ctx, _, value):
+    if not value:
+        return
+    else:
+        click.echo(f"fmdpy: {VERSION} ({sys.executable})")
+        ctx.exit()
+
+
 # This is needed for cli (too-many-arguments and too-many-locals)
 # pylint: disable=too-many-arguments disable=too-many-locals
 @click.command()
+
 @click.option('-c', "--count", default=int(config['UI']['max_result_count']),
               help="Max Number of results")
+
 @click.option('-f', "--fmt", default=config['DL_OPTIONS']['fmt'],
               help="Format of the audio file.")
+
 @click.option('-b', "--bitrate", default=int(config['DL_OPTIONS']['bitrate']),
               help="Bitrate in kb, (250 is default)")
+
 @click.option('-m', "--multiple",
               default=int(config['DL_OPTIONS']['multiple']),
               help="number of concurrent downloads.")
+
 @click.option('-d', "--directory",
               default=config['DL_OPTIONS']['default_directory'],
               help="Specify the folder.", type=click.Path(exists=True))
+
 @click.option('-l', "--lyrics", help="Add lyrics",
               default=ast.literal_eval(config['DL_OPTIONS']['lyrics']),
               is_flag=True)
-@click.option('-V', "--Version", help="display version", is_flag=True)
-@click.option('-g', "--gen", help="generate the config file.", is_flag=True)
-@click.option('-u', "--update", help="Update: (for pip only)", is_flag=True)
+
+@click.option('-g', "--gen", help="generate the config file.", is_flag=True,
+        callback=gen, expose_value=False, is_eager=True)
+
+@click.option('-u', "--update", help="Update: (for pip only)", is_flag=True,
+        callback=update, expose_value=False, is_eager=True)
+
+@click.option('-v', "--version", help="display version", is_flag=True,
+        callback=version, expose_value=False, is_eager=True)
+
 @click.argument('search', nargs=-1)
+
 def fmdpy(count, search, fmt, bitrate, multiple,
-          version, lyrics, update, directory, gen):
+        lyrics, directory):
     """FMDPY.
 
     Download music with metadata\n
@@ -62,22 +103,6 @@ def fmdpy(count, search, fmt, bitrate, multiple,
     -f native: save to native container [Default](ffmpeg not req.)
     (-b is ignored)
     """
-    if update:
-        subprocess.check_call([sys.executable, '-m', 'pip',
-                              'install', '--upgrade', *install_requires])
-        sys.exit(0)
-
-    if version:
-        print("fmdpy:", VERSION, f'({sys.executable})')
-        sys.exit(0)
-
-    if gen:
-        file_path = os.getenv('FMDPY_CONFIG_FILE') or \
-            os.path.expanduser('~/.fmdpy.ini')
-        with open(file_path, 'w', encoding='utf8') as configfile:
-            config.write(configfile)
-        sys.exit(0)
-
     search = ' '.join(search)
     if 'spotify.com/playlist' in search:
         pl_spotify_dl(search, dlformat=fmt, bitrate=bitrate,
