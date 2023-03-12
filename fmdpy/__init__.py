682b418583fc63ea1dd16b6f71e21a31e2719150
Rohn Chatterjee <rohnch@protonmail.ch>
2023-03-12 10:14:39 +0530

    Update User-Agent

--- fmdpy/__init__.py
+++ fmdpy/__init__.py
@@ -6,8 +6,8 @@ install_requires = ['ffmpeg-python', 'click', 'music-tag>=0.4.3', 'requests',
                     'pillow', 'lyricsgenius', 'dataclasses', 'spotipy', 'tqdm']
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1;' +
-    ' WOW64; rv:39.0) Gecko/20100101 Firefox/75.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
+    '(KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 }
 
 ART = r'''  _| |_ |  \ /  |   /\  (   _   ) || || |
