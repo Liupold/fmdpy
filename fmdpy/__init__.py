5fa98ff1367045cc58e1e6a8f7f928292aaedae7
Rohn Chatterjee <rohnch@protonmail.ch>
2023-04-21 12:37:31 +0530

    version bump.

--- fmdpy/__init__.py
+++ fmdpy/__init__.py
@@ -1,7 +1,7 @@
 """fmdpy."""
 from . import conf
 
-VERSION = "0.6.3"
+VERSION = "0.7"
 install_requires = ['click', 'music-tag>=0.4.3', 'requests', 'pillow',
                     'lyricsgenius', 'dataclasses', 'spotipy', 'tqdm', 'pydub',
                     'pycryptodome', 'appdirs']
